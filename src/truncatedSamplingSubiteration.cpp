b2ea12ac9945f4871b3b2e5052e4d07314be6b9d
Alexandra Sarafoglou <alexandra.sarafoglou@gmail.com>
2022-09-14 15:50:09 +0200

    Update truncatedSamplingSubiteration.cpp

--- src/truncatedSamplingSubiteration.cpp
+++ src/truncatedSamplingSubiteration.cpp
@@ -1,7 +1,7 @@
 
 #include <Rcpp.h>
-//#include <mpfr.h>
-#include "/opt/homebrew/Cellar/mpfr/4.1.0/include/mpfr.h"
+#include <mpfr.h>
+//#include "/opt/homebrew/Cellar/mpfr/4.1.0/include/mpfr.h"
 
 
 using namespace Rcpp;
